fdff794703258e904b7420890813e9cbab266654
DaltonPederson <144394745+DaltonPederson@users.noreply.github.com>
2023-09-20 23:44:12 -0500

    code for the appletree
    Apple tree code added. The tree is now moving from left to right while never leaving the screen

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject ApplePrefab;
+
+    //Speed at which AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree moves
+    public float leftandRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which AppleTree will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
-    {
+    { //Dropping Apples every second 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movement
+        Vector3 pos =
+    transform.position;
+        pos.x += speed *
+    Time.deltaTime;
+        transform.position =
+    pos;
+        //Changing Direction
+        if (pos.x < -leftandRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftandRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
     }
 }
