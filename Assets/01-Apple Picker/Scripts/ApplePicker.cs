0a268123ffa841ea51f42d6cf9f81a14363967f9
DaltonPederson <144394745+DaltonPederson@users.noreply.github.com>
2023-09-21 23:43:08 -0500

    final code
    All of a sudden, everything is wrong, even though once again it is by the book.

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -10,22 +10,50 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        basketList = new List<GameObject>();
         for (int i=0; i<numBaskets; i++)
         {
             GameObject tBasketGO =
-                Instantiate<GameObject>(basketPrefab);
+                Instantiate<GameObject>(BasketPrefab);
             Vector3 pos = Vector3.zero;
-            pos.y = basketBottomY + (basketSpacingY * i);
+            pos.y = BasketBottomY + (BasketSpacingY * i);
             tBasketGO.transform.position = pos;
+
+            basketList.Add(tBasketGO);
         }
     }
 
+    public void AppleDestroyed()
+
+        GameObject[]
+
+        tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+
+    foreach (GameObject tGO in tAppleArray) {
+        Destroy(tGO);
+    }
+
+int basketIndex = basketList.Count - 1;
+
+GameObject tBasketGO =
+    basketList[basketIndex];
+
+basketList.RemoveAt(basketIndex);
+Destroy(tBasketGO);
+
+if (basketList.Count == 0)
+{
+
+    SceneManager.LoadScene("ApplePickerDemo");
+}
+
     // Update is called once per frame
     void Update()
-    {
-        
+
+
     }
 }
--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set dynamically")]
+
+    public Text
+
+          scoreGT;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() { 
+        //Find a reference to the ScoreCounter GameObject
+
+        GameObject scoreGO =
+
+        GameObject.Find("ScoreCounter");
+
+        scoreGT =
+
+            scoreGO.GetComponent<Text>();
+
+        scoreGT.text = "0";  
         
     }
 
@@ -42,6 +59,19 @@ public class Basket : MonoBehaviour
 
             {
                 Destroy(collidedWith);
+
+                int score =
+
+                int.Parse(scoreGT.text);
+
+                score += 100;
+
+                scoreGT.text = score.ToString();
+
+                if (score > HighScore.score)
+                {
+                    HighScore.score = score;
+                }
             }
             }
     }
