c9ae81a0935a2cfe0658f204c183dcb34ba9f10f
DaltonPederson <144394745+DaltonPederson@users.noreply.github.com>
2023-09-21 22:33:42 -0500

    apple pick up
    added the code for picking up the apples. for some reason, the code is not registering correctly, even though it is by the book.

---
+++ Assets/01-Apple Picker 1/Scripts/AppleTree.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject ApplePrefab;
+
+    //Speed at which AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree moves
+    public float leftandRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which AppleTree will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    { //Dropping Apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject Apple =
+
+        Instantiate<GameObject>(ApplePrefab);
+
+        Apple.transform.position =
+
+            transform.position;
+
+        Invoke("DropApple", secondsBetweenAppleDrops);
+
+    }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Basic Movement
+            Vector3 pos =
+        transform.position;
+            pos.x += speed *
+        Time.deltaTime;
+            transform.position =
+        pos;
+            //Changing Direction
+            if (pos.x < -leftandRightEdge)
+            {                   //a
+                speed = Mathf.Abs(speed); //Move right
+                                          //b
+            }
+            else if (pos.x > leftandRightEdge)
+            {       //c
+                speed = -Mathf.Abs(speed); //Move left
+                                           //c
+            }
+            else if (Random.value < chanceToChangeDirections)
+            {
+
+
+                speed *= -1;
+
+
+            }
+        }
+
+}
+
